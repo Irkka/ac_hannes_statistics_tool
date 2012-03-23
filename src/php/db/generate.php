8a50c64c34fa9aea34f33a69ddf0e00c3b36379d
Ilkka Hakkarainen <ilkkahak@eva.(none)>
2012-03-23 10:58:37 +0200

    Front page evolving

---
+++ src/php/db/generate.php
@@ -0,0 +1,18 @@
+<?php
+    /**
+     * number of players, teams, events and other to create
+     */
+    $players = rand(10,20);
+    $opponents = rand(7,15);
+//SEURAAVAT TÄYTYY MIETTIÄ VÄHÄN TARKEMMIN
+
+    /*== read in the two files into the arrays ==*/
+    $adj_array = file("wu_adjs");
+    $noun_array = file("wu_nouns");
+    $team_prefix = file("wu_team_pre");
+    $team_suffix = file("wu_team_suf");
+
+    /*== set random seed ==*/ 
+    $seed = rand(0, 20000);
+    srand($seed);
+?>
--- src/php/index.php
+++ src/php/index.php
@@ -1,7 +1,40 @@
 <?php
 include 'templates/upper.html';
 //$default_query="SELECT (player_number, last_name, (select count(*) FROM statistics_event WHERE item_id=1 AND player_id=1)) FROM player WHERE player_id=1";
-include ("db/populate.php");
+include('db/connect_db.php');
+$query = $db->prepare("SELECT player_number, last_name, first_name, (SELECT COUNT(*) FROM statistics_event WHERE item_id = :item_id AND player.player_id = statistics_event.player_id) FROM player ORDER BY player_number");
+$query->bindParam(':item_id', $item_id);
+$query->bindParam(':player_number', $player_number);
+$query->bindParam(':last_name', $last_name);
+$query->bindParam(':first_name', $first_name);
+$query->bindParam(':where', $where);
 
-include 'templates/lower.html'
+//Näihin voi laittaa kyselyt WHERE item_id on dropboxissa olevan itemin nimi
+$item_id = 1;
+$player_number = '';
+$last_name = '';
+$first_name = '';
+$where = '';
+?>
+<table>
+<?php
+//var_dump($query);
+$query->execute();
+$result = $query->fetchAll();
+//var_dump($result);
+
+foreach($result as $row) {
+    print('<tr>');
+    print('<td>' . $row['player_number'] . '</td>');
+    print('<td>' . $row['last_name'] . '</td>');
+    print('<td>' . $row['first_name'] . '</td>');
+    print('<td>' . $row[3] . '</td>');
+    print('</tr>');
+}
+
+?>
+</table>
+<?php
+include('db/close_db.php');
+include 'templates/lower.html';
 ?>
